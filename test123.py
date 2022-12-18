6f95bb9c8e2e38b201c5620a6298c5b9e19fea5a
babu-Ahmed <120332237+babu-Ahmed@users.noreply.github.com>
2022-12-18 10:22:17 -0500

    this is an example program
     I am printing the minumum number in nums list

--- test123.py
+++ test123.py
@@ -1,6 +1,12 @@
+# this is an example program
 myoxford={
 "apple":"a fruit",
 "computer":"a machine",
 24:"roll no"
 }
 print(myoxford)
+
+nums = [22,33,44,55]
+
+print("the minimum in", nums, "is", min(nums))
+# I am printing the minumum number in nums list
