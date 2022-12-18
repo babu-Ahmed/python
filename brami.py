1fb4ef59d03c62a0d078f07db9cb113e40dbe04d
babu-Ahmed <120332237+babu-Ahmed@users.noreply.github.com>
2022-12-18 10:14:58 -0500

    visual_code_not_necessary
    to code in python, you don't need a IDE;
    you can use notepad++, cmd

--- brami.py
+++ brami.py
@@ -4,3 +4,7 @@ c=30
 print(a+b)
 print(a+c)
 print(10**2)
+print(5*30)
+
+print("a is positive number")
+
---
+++ test123.py
@@ -0,0 +1,6 @@
+myoxford={
+"apple":"a fruit",
+"computer":"a machine",
+24:"roll no"
+}
+print(myoxford)
